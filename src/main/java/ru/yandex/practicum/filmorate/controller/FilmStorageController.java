368bdf671af383f263a2f587aeb22d60ab4d040d
IG <iassiriya@yandex.ru>
2025-03-29 12:26:25 +0300

    Финальное ТЗ спринт 10 правки 4

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmStorageController.java
+++
@@ -1,6 +0,0 @@
-package ru.yandex.practicum.filmorate.controller;
-
-import ru.yandex.practicum.filmorate.model.Film;
-
-public class FilmStorageController extends AbstractController<Film> {
-}
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserStorageController.java
+++
@@ -1,6 +0,0 @@
-package ru.yandex.practicum.filmorate.controller;
-
-import ru.yandex.practicum.filmorate.model.User;
-
-public class UserStorageController extends AbstractController<User>  {
-}
